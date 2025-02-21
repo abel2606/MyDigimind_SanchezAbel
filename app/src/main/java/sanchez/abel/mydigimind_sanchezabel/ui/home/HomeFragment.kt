f2ff74f5c9009e2f8176cc2492359cab208b790b
abel2606 <abel.sanchez.guerrero@gmail.com>
2025-02-20 21:45:43 -0700

    Vista de la pantalla principal

--- app/src/main/java/sanchez/abel/mydigimind_sanchezabel/ui/home/HomeFragment.kt
+++ app/src/main/java/sanchez/abel/mydigimind_sanchezabel/ui/home/HomeFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.GridView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
@@ -20,6 +21,7 @@ import sanchez.abel.mydigimind_sanchezabel.ui.dashboard.DashboardViewModel
 class HomeFragment : Fragment() {
 
     var tasks = ArrayList<Task>()
+    private var adaptador: AdaptadorTareas? = null
     private lateinit var  homeViewModel: HomeViewModel
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -30,6 +32,12 @@ class HomeFragment : Fragment() {
             ViewModelProvider(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
         fillTasks()
+
+        adaptador = AdaptadorTareas(root.context, tasks)
+
+        val gridView: GridView = root.findViewById(R.id.gridView)
+
+        gridView.adapter = adaptador
         return root
     }
 
@@ -45,8 +53,43 @@ class HomeFragment : Fragment() {
     }
 
     private class AdaptadorTareas: BaseAdapter{
-        var task = ArrayList<Task>()
-        var contexto = Context?
+        var tasks = ArrayList<Task>()
+        var contexto: Context? = null
+
+        constructor(contexto: Context, tasks: ArrayList<Task>){
+            this.contexto = contexto
+            this.tasks = tasks
+        }
+
+        override fun getCount(): Int {
+            return tasks.size
+        }
+
+        override fun getItem(position: Int): Any {
+            return tasks[position]
+        }
+
+        override fun getItemId(position: Int): Long {
+            return position.toLong()
+        }
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+            var task = tasks[position]
+            var inflador = LayoutInflater.from(contexto)
+            var vista = inflador.inflate(R.layout.task_view, null)
+
+            var tv_title: TextView = vista.findViewById(R.id.tv_title)
+            var tv_time: TextView = vista.findViewById(R.id.tv_time)
+            var tv_day: TextView = vista.findViewById(R.id.tv_days)
+
+            tv_title.setText(task.title)
+            tv_time.setText(task.time)
+            tv_day.setText(task.days.toString())
+
+            return vista
+
+        }
+
 
     }
 
